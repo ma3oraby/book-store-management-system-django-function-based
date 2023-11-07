99dcc00b4ba9e9d8a6aadb67c11280976ab16a04
unknown <ma3oraby@gmail.com>
2023-11-07 21:06:02 +0200

    customize add category form in index.html

--- bsms_app/forms.py
+++ bsms_app/forms.py
@@ -1,5 +1,13 @@
 from django import forms 
-from .models import Book
+from .models import Book , Category
+
+class CategoryForm (forms.ModelForm):
+    class Meta :
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name' : forms.TextInput(attrs={'class':'form-control'})
+        }
 
 class BookForm (forms.ModelForm):
     class Meta : 
--- bsms_app/views.py
+++ bsms_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book , Category 
-from .forms import BookForm
+from .forms import BookForm , CategoryForm
 
 # Create your views here.
 
@@ -9,12 +9,17 @@ def index (request) :
         add_book = BookForm(request.POST , request.FILES)
         if add_book.is_valid() :
             add_book.save()
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid() :
+            add_category.save()
+
 
 
     context = {
         "categories" : Category.objects.all(),
         "books" : Book.objects.all(), 
         "form" : BookForm(),
+        "formcat" : CategoryForm(),
     }
     return render (request , 'bsms_app/index.html',context)
 
